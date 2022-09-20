a94e2f5c3cb48ca8aedddbaa60f0427fa765d868
Greg <gjmooney@asu.edu>
2022-09-20 13:18:48 +0200

    Refactor out menu class

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -10,57 +10,12 @@ public class Menu {
     public static void main(String[] args) {
 
         Connection connection = null;
-        int choice = 0;
-        Scanner input = new Scanner(System.in);
-        boolean exit = false;
-        
 
         // Connect to DB
         try {
             connection = connect(args[0], args[1], args[2], args[3]);
-
-        // Display menu
-        do {
-            System.out.println();
-            System.out.println("Welcome to Games DB");
-            System.out.println("Please select a valid option (1, 2, 3, 4, or 5).");
-            System.out.println("1. Start a Query");
-            System.out.println("2. Insert a DB entry");
-            System.out.println("3. Update a DB entry");
-            System.out.println("4. Delete a DB entry");
-            System.out.println("5. Quit");
-            System.out.println();
-
-            choice = input.nextInt();
-            switch (choice) {
-                case 1:
-                    
-                    break;
-                case 2:
-                    
-                    break;
-                case 3:
-                    
-                    break;
-                case 4:
-                    
-                    break;
-                case 5:
-                    System.out.println("BUH BYE!");
-                    input.close();
-                    exit = true;
-                    break;
             
-                default:
-                    break;
-            }
-        } while (!exit);
-            
-        } catch (InputMismatchException e) {
-            input = new Scanner(System.in);
-            System.out.println("Please enter a valid selection (1, 2, 3, 4, or 5)");
-        } 
-        catch (SQLException e) {
+        } catch (SQLException e) {
             // TODO: handle exception
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
@@ -75,6 +30,53 @@ public class Menu {
                 // TODO: handle exception
             }
         }        
+    }
+
+    public static void displayMenu() {
+        int choice = 0;
+        Scanner input = new Scanner(System.in);
+        boolean exit = false;
+
+        try {
+            do {
+                System.out.println();
+                System.out.println("Welcome to Games DB");
+                System.out.println("Please select a valid option (1, 2, 3, 4, or 5).");
+                System.out.println("1. Start a Query");
+                System.out.println("2. Insert a DB entry");
+                System.out.println("3. Update a DB entry");
+                System.out.println("4. Delete a DB entry");
+                System.out.println("5. Quit");
+                System.out.println();
+    
+                choice = input.nextInt();
+                switch (choice) {
+                    case 1:
+                        
+                        break;
+                    case 2:
+                        
+                        break;
+                    case 3:
+                        
+                        break;
+                    case 4:
+                        
+                        break;
+                    case 5:
+                        System.out.println("BUH BYE!");
+                        input.close();
+                        exit = true;
+                        break;
+                
+                    default:
+                        break;
+                }
+            } while (!exit);
+        } catch (InputMismatchException e) {
+            input = new Scanner(System.in);
+            System.out.println("Please enter a valid selection (1, 2, 3, 4, or 5)");
+        }
     }
 
     public static Connection connect(String url, String user, String password, String driver) throws SQLException, ClassNotFoundException{
